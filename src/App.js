e73ebc0d2838f3698ee5aaa7ad9e3d89c993b218
masahitojp <randomstep@gmail.com>
2018-04-05 00:25:08 +0900

    chore: temp

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Button } from './components';
+
+
+const App = () => (
+  <Button> test </Button>
+);
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-require('./app.css');
-
-
-class App extends React.Component {
-	render() {
-		return (
-			<div className="container">
-				<h1>Hello React!</h1>
-			</div>
-		);
-	}
-}
-
-ReactDOM.render(
-	<App />,
-	document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
