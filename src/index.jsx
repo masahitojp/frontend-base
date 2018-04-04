f3944fc4e90a0b53d2fe651ca1748411e44a16db
masahitojp <randomstep@gmail.com>
2018-04-04 23:50:36 +0900

    create project

---
+++ src/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+require('./app.css');
+
+
+class App extends React.Component {
+	  render() {
+		      return (
+			            <div className="container">
+			              <h1>Hello React!</h1>
+			            </div>
+			          );
+		    }
+}
+
+ReactDOM.render(
+	  <App />,
+	  document.getElementById('root'),
+);
