d330d680975ad55871ec6cf5d6c74a2f2d72929e
masahitojp <randomstep@gmail.com>
2018-04-05 09:14:44 +0900

    feat: add storybook setting

---
+++ .storybook/config.js
@@ -0,0 +1,4 @@
+import { configure } from '@storybook/react';
+const req = require.context('../src/components', true, /.stories.js$/)
+const loadStories = () => req.keys().forEach((filename) => req(filename))
+configure(loadStories, module);
