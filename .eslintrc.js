e1cb500a069ad1a0662fb464412de0c8a3a7662a
masahitojp <randomstep@gmail.com>
2018-04-30 21:59:50 +0900

    chore: add eslint-plugin-jest and add settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,10 +7,17 @@ module.exports = {
     'prettier/flowtype',
     'prettier/react',
   ],
-  plugins: ['flowtype', 'prettier'
-  ],
+  plugins: ['flowtype', 'prettier', 'jest'],
   rules: {
     'react/require-default-props': 0,
     'react/no-unused-prop-types': 0,
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+  },
+  env: {
+    'jest/globals': true,
   },
 }
