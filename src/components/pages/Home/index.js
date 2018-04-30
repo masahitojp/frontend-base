d5e9dc19ba6e6d26a327db29d2088fc353f0ad52
masahitojp <randomstep@gmail.com>
2018-04-30 23:08:07 +0900

    feat: add some pages

---
+++ src/components/pages/Home/index.js
@@ -0,0 +1,12 @@
+/* @flow */
+import React, { Fragment } from 'react'
+import Navbar from '../../molecules/NavBar'
+
+const Home = () => (
+  <Fragment>
+    <Navbar />
+    <h1>Hello!</h1>
+  </Fragment>
+)
+
+export default Home
