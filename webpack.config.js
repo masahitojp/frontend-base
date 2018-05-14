5f9b2c6781c671b36604ffffb8af9df87a55a7f9
masahitojp <randomstep@gmail.com>
2018-05-15 00:36:13 +0900

    feat: :wrench: migrate settings for webpack-serve

--- webpack.config.js
+++ webpack.config.js
@@ -2,37 +2,34 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-	entry: {
-		'index': [
-			path.resolve(__dirname, 'src/index.js')
-		]
-	},
-	output: {
-		path: path.resolve(__dirname, './dist'),
-		filename: 'bundle.js',
-	},
-	devServer: {
-		contentBase: path.resolve(__dirname, './dist'),
-		port: 3000,
-		publicPath: '/',
-	},
-	devtool: 'eval-source-map',  // デプロイするときには削除
-	mode: 'development',
-	plugins: [
-		new webpack.DefinePlugin({
-			'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-		})
-
-	],
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				use: 'babel-loader',
-			},
-		]
-	},
-	resolve: {
-		extensions: ['.js', '.jsx'],
-	},
-};
+  entry: {
+    index: [path.resolve(__dirname, 'src/index.js')],
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+  serve: {
+    content: path.resolve(__dirname, './dist'),
+    port: 3000,
+    dev: { publicPath: '/' },
+  },
+  devtool: 'eval-source-map', // デプロイするときには削除
+  mode: 'development',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+}
