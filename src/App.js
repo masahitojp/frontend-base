36bab037e420102cd56b142174aaf18c2db6cacd
masahitojp <randomstep@gmail.com>
2018-09-29 22:27:12 +0900

    feat: :sparkles: add@lingui

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
+import { I18nProvider } from '@lingui/react'
 import Routes from './Routes'
 import createStore from './store/createStore'
+
+import catalogEn from '../locale/en/messages.js'
 
 const App = () => {
   const store = createStore()
   return (
     <BrowserRouter>
-      <Provider store={store}>
-        <Routes />
-      </Provider>
+      <I18nProvider language="en" catalogs={{ en: catalogEn }}>
+        <Provider store={store}>
+          <Routes />
+        </Provider>
+      </I18nProvider>
     </BrowserRouter>
   )
 }
--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -1,11 +1,13 @@
 /* @flow */
 import React, { Fragment } from 'react'
+import { Trans } from '@lingui/react'
 import Navbar from '../../molecules/NavBar'
 
 const Home = () => (
   <Fragment>
     <Navbar />
     <h1>Hello!</h1>
+    <Trans id="Hello" />
   </Fragment>
 )
 
